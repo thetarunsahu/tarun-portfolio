39f5844df78dde46cf640ce1b30d1b3bcb7895c1
Tarun Kumar Sahu <tarunkumarsahu354@gmail.com>
2026-09-02 04:36:50 +0530

    Load editorial media styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./experience.css";
+import "./media.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
