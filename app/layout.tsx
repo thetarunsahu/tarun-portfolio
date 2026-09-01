d7d1b6287c2dbd29f5032d9105a80900264aa26e
Tarun Kumar Sahu <tarunkumarsahu354@gmail.com>
2026-09-02 04:38:22 +0530

    Load subpage art direction styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./experience.css";
 import "./media.css";
+import "./pages.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
